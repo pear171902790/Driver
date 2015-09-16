8381d20ea3098729af08c9c3157038e3590ea34c
Peter Lee <lic@shinetechchina.com>
2015-09-16 10:59:55 +0800

    12

---
+++ Driver/DriverDBContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driver
+{
+    
+    public class DriverDBContext : DbContext
+    {
+        public DriverDBContext()
+            : base("name=DriverDB")
+        {
+
+        }
+        public DbSet<Data> Datas { get; set; }
+    }
+
+    public class Data
+    {
+        [Key]
+        public Guid Key { get; set; }
+        public int Type { get; set; }
+        public string Value { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreateTime { get; set; }
+        public bool Valid { get; set; }
+    }
+
+    public enum DataType
+    {
+        User = 1,
+        Position = 2
+    }
+}
