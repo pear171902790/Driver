fb4014f9857175eabb736ce8cd3e8d871623b8e1
Peter Lee <lic@shinetechchina.com>
2015-09-16 10:29:35 +0800

    11

---
+++ Driver/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Driver.Startup))]
+namespace Driver
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
